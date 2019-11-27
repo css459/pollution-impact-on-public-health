2e7f25d5b5127782ef4a279ef37ec0d4aff2e511
Cole Smith <css@nyu.edu>
2019-11-26 19:00:26 -0500

    AQI and TRI loaders

--- src/etl/load.py
+++ src/etl/load.py
@@ -3,3 +3,72 @@
 # Cole Smith
 # load.py
 #
+
+import glob
+import os
+
+import pandas as pd
+
+RAW_DATA = 'data/raw/'
+
+
+def _load(subpath):
+    path = os.path.join(RAW_DATA, subpath, '*.csv')
+    all_files = glob.glob(path)
+
+    li = []
+    for filename in all_files:
+        df = pd.read_csv(filename, index_col=None)
+        li.append(df)
+
+    return pd.concat(li, axis=0, ignore_index=True)
+
+
+def load_tri():
+    """
+    Loads the TRI CSV files into one DataFame
+
+    :return: DataFrame
+    """
+    # Remove NA columns
+    combined = _load('tri').dropna(axis=1)
+
+    # Clean column names
+    cols = combined.columns
+    cols = [c.split(' - ')[-1] for c in cols]
+    cols = [c.split('. ')[-1].lower() for c in cols]
+    combined.columns = cols
+
+    return combined
+
+
+def load_aqi():
+    """
+    Loads the AQI CSV files into one DataFame
+
+    :return: DataFrame
+    """
+    # Remove NA columns
+    combined = _load('aqi').dropna(axis=1)
+
+    # Select relevant columns
+    sel = ['State Name', 'county Name', 'Date', 'AQI', 'Defining Parameter']
+    combined = combined[sel]
+
+    # Clean column names
+    combined.columns = [c.replace(" ", "_").lower() for c in combined.columns]
+
+    # Convert date column to year only
+    combined.date = combined.date.str.split('-').str[0]
+    combined = combined.rename(columns={'date': 'year'})
+    combined = combined.astype({'year': 'int64'})
+
+    # Pivot defining_parameter column
+    d = pd.get_dummies(combined['defining_parameter'], prefix='defining')
+    df = pd.concat([combined, d], axis=1).drop(['defining_parameter'], axis=1)
+
+    return df
+
+
+if __name__ == '__main__':
+    out = load_aqi()
