d460b9c4234cccaa020ef32c4213a4765157b650
Cole Smith <css@nyu.edu>
2019-12-02 14:31:31 -0500

    SMA EMA Featurizer

--- src/main.py
+++ src/main.py
@@ -1,19 +1,38 @@
 from src.etl.load import load_all
 from src.etl.preprocess import split
+from src.model.features import sma_featurizer
+
+# Load the merged data set
+df = load_all()
+
+# Featurize the merged data by add the Exponential
+# Moving Average for each column for 1, 2, and 3 year
+# windows as separate columns
+df = sma_featurizer(df)
 
 # Scale, shuffle, split the merged data
-df = load_all()
-train, test = split(df)
+# We will consider the "cancer" columns
+# and "age-adjusted_rate" to be Y columns
+y_cols = ['age-adjusted_rate'] + [c for c in df.columns if "cancer" in c]
+x_cols = [c for c in df.columns if c not in y_cols]
 
-# Visualize the initial clusters of age-adjusted cancer
-# rates for each area. We will group each area
-# by adding up all the years observed
-from src.model.cluster import view_pca
+print("Considering X Columns:")
+print(x_cols)
 
-grouped = df.groupby(by=['lat', 'lon']).sum()
-cancer_cols = [c for c in df.columns if "cancer" in c]
-print(cancer_cols)
-view_pca(grouped.drop(['year'] + cancer_cols, 1), 'age-adjusted_rate',
-         title="All Merged Data, Colored by Age-Adjusted Cancer Rates",
-         xlabel="First Component",
-         ylabel="Second Component")
+print("Considering Y Columns:")
+print(y_cols)
+
+x_train, y_train, x_test, y_test = split(df, y_cols=y_cols)
+
+# # Visualize the initial clusters of age-adjusted cancer
+# # rates for each area. We will group each area
+# # by adding up all the years observed
+# from src.model.cluster import view_pca
+#
+# grouped = df.groupby(by=['lat', 'lon']).sum()
+# cancer_cols = [c for c in df.columns if "cancer" in c]
+# print(cancer_cols)
+# view_pca(grouped.drop(cancer_cols, 1), 'age-adjusted_rate',
+#          title="All Merged Data w. EMA, Colored by Age-Adjusted Cancer Rates\n\n",
+#          xlabel="First Component",
+#          ylabel="Second Component")
--- src/model/features.py
+++ src/model/features.py
@@ -3,3 +3,55 @@
 # Cole Smith
 # features.py
 #
+
+
+def sma_featurizer(df, by=None, lags=None, ema=True):
+    """
+    Computes the SMA of each column, grouped by
+    the `by` set. This follows for each `lag` in
+    `lags`. The default `by` is `[lat, lon]`. The
+    default `lags` is `[1,2,3]`, which represent years
+    in the merged dataset in this project. SMA
+    columns will be appended with prefix smaN_.
+
+    :param df:      DataFrame to featurize
+    :param by:      Grouping columns (default: [lat, lon]
+    :param lags:    SMA lag periods (default: [1,2,3])
+    :param ema:     Use exponential moving averages
+    :return:        Featurized DataFrame
+    """
+    # Resolve defaults
+    if by is None:
+        by = ['lat', 'lon']
+    if lags is None:
+        lags = [1, 2, 3]
+
+    # Drop year if we're grouping by lat and lon
+    if 'lat' in by and 'lon' in by:
+        df = df.drop('year', 1)
+
+    g = df.groupby(by=by)
+
+    # Define an inner convenience function
+    def roll(l):
+        if ema:
+            r = g.sum().ewm(l).mean()
+            r.columns = ["ema" + str(l) + "_" + c for c in r]
+        else:
+            r = g.expanding(l).sum()
+            r.columns = ["sma" + str(l) + "_" + c for c in r]
+        return r
+
+    features = roll(lags[0])
+    for lag in lags[1:]:
+        tmp = roll(lag)
+        features[tmp.columns] = tmp
+
+    # Left join SMA/EMA features on original DataFrame
+    features_indexes = features.index
+    features = features.reset_index()
+    for i in range(len(by)):
+        b = by[i]
+        features[b] = [ind[i] for ind in features_indexes]
+
+    return df.merge(features, left_on=by, right_on=by)
